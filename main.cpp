478a7886b856c891ef9588dc0b6a60ca314b23ac
Niko Dittmar <nikodittmar@gmail.com>
2024-10-26 10:05:47 -0700

    Added benchmark class

--- main.cpp
+++ main.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
 #include <chrono>
 using namespace std;
+using namespace std::chrono;
+
+class Benchmark {
+private: 
+    time_point<high_resolution_clock> start_time;
+    time_point<high_resolution_clock> end_time;
+    int milliseconds;
+public:
+    Benchmark() {
+        milliseconds = -1;
+    }
+    int getTime() {
+        return milliseconds;
+    }
+    void startTimer() {
+        start_time = high_resolution_clock::now();
+    }
+    void endTimer() {
+        end_time = high_resolution_clock::now();
+        milliseconds = duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
+    }
+};
 
 int main() {
     
-
+    
     return 0;
 }
 
