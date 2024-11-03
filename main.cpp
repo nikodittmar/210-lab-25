4c6ddb2f9d462feeca445e9926f2a321c72f71aa
Niko Dittmar <nikodittmar@gmail.com>
2024-11-03 11:56:16 -0800

    Moved testing to a function

--- main.cpp
+++ main.cpp
@@ -41,6 +41,8 @@ public:
     }
 };
 
+vector<vector<int>> runTest();
+
 int main() {
 
     list<string> list;
@@ -138,9 +140,98 @@ int main() {
     return 0;
 }
 
-/* syntax examples:
-auto start = high_resolution_clock::now()
-auto end = high_resolution_clock::now()
-auto duration = duration_cast<milliseconds>(end - start)
-duration.count() references elapsed milliseconds
-*/
+vector<vector<int>> runTest() {
+    list<string> list;
+    set<string> set;
+    vector<string> vector;
+
+    string value;
+
+    // READ
+
+    ifstream fin1("codes.txt");
+    Benchmark vectorRead;
+    vectorRead.startTimer();
+    while(fin1 >> value) {
+        vector.push_back(value);
+    }
+    vectorRead.endTimer();
+    fin1.close();
+    
+    ifstream fin2("codes.txt");
+    Benchmark listRead;
+    listRead.startTimer();
+    while(fin2 >> value) {
+        list.push_back(value);
+    }
+    listRead.endTimer();
+    fin2.close();
+
+    ifstream fin3("codes.txt");
+    Benchmark setRead;
+    setRead.startTimer();
+    while(fin3 >> value) {
+        set.insert(value);
+    }
+    setRead.endTimer();
+    fin3.close();
+
+    // SORT
+    Benchmark vectorSort;
+    vectorSort.startTimer();
+    sort(vector.begin(), vector.end());
+    vectorSort.endTimer();
+
+    Benchmark listSort;
+    listSort.startTimer();
+    list.sort();
+    listSort.endTimer();
+
+    // INSERT
+    string toAdd = "TESTCODE";
+    
+    Benchmark vectorInsert;
+    vectorInsert.startTimer();
+    vector.insert(vector.begin() + 10000, toAdd);
+    vectorInsert.endTimer();
+
+    Benchmark listInsert;
+    listInsert.startTimer();
+    std::list<string>::iterator it = list.begin();
+    std::advance(it, 10000);
+    list.insert(it, toAdd);
+    listInsert.endTimer();
+
+    Benchmark setInsert;
+    setInsert.startTimer();
+    set.insert(toAdd);
+    setInsert.endTimer();
+
+    // DELETE
+
+    Benchmark vectorDelete;
+    vectorDelete.startTimer();
+    vector.erase(vector.begin() + 10000);
+    vectorDelete.endTimer();
+
+    Benchmark listDelete;
+    listDelete.startTimer();
+    std::list<string>::iterator it2 = list.begin();
+    std::advance(it2, 10000);
+    list.erase(it2);
+    listDelete.endTimer();
+
+    Benchmark setDelete;
+    setDelete.startTimer();
+    set.erase(toAdd);
+    setDelete.endTimer();
+
+    std::vector<std::vector<int>> results = {
+        { vectorRead.getTime(), listRead.getTime(), setRead.getTime() },
+        { vectorSort.getTime(), listSort.getTime(), -1 },
+        { vectorInsert.getTime(), listInsert.getTime(), setInsert.getTime() },
+        { vectorDelete.getTime(), listDelete.getTime(), setDelete.getTime() }
+    };
+
+    return results;
+}
